7d2dabaf85ff19a9bf1d524767f481aa656b41bb
Matyas Bar <ma1y0@pop-os.localdomain>
2023-03-25 13:56:50 +0100

    cp command

--- cp/src/main.rs
+++ cp/src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+use std::fs;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(index = 1)]
+    from: String,
+    #[arg(index = 2)]
+    to: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    
+    match fs::copy(args.from, args.to) {
+        Ok(_) => println!("File succesfully copied"),
+        Err(e) => println!("Failed to copy the file with error: {}", e)
+    }
+
 }
